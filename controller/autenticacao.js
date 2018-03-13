815778165e0697998358509195e7da9825eb4e10
Douglas Antunes <douglasantunesbarreto@gmail.com>
2018-03-13 16:28:15 -0300

    Autenticacao erro customizacor

--- controller/autenticacao.js
+++ controller/autenticacao.js
@@ -25,18 +25,10 @@ app2.controller("autenticacao", function ($scope, $http, $rootScope) {
                 $scope.user = data.data[0];
                 if ($scope.user.bloqueado == 1) return Materialize.toast("Usuário Bloqueado, entrar em contato!", 4000, 'red darken-1');
                 dir = ($scope.user.customiza == 1) ? "../customizador/" : "../";
-                $scope.user = data.data[0];
-                $http.post(URL + "/leArquivo/", { arquivo: $scope.user.projeto.toUpperCase() + "/inicial.html" }).success(function (data) {
-                    // if (data != "" && $scope.user.customiza == 0) {
-                    //     // Sistema do cliente
-                    //     $scope.user.sysCli = true;
-                    //     window.location = $scope.user.projeto.toUpperCase() + "/inicial.html?";
-                    // }
-                    // else 
-                    window.location = dir + "inicial.html?";
-                    $scope.user.logado = { id: $scope.user.id, projeto: $scope.user.projeto, projeto_id: $scope.user.projeto_id, banco: $scope.user.banco, nao_saas: $scope.user.nao_saas };
-                    localStorage.user = JSON.stringify($scope.user);
-                });
+                $scope.user.sysCli = true;
+                window.location = "/inicial.html?";
+                $scope.user.logado = { id: $scope.user.id, projeto: $scope.user.projeto, projeto_id: $scope.user.projeto_id, banco: $scope.user.banco, nao_saas: $scope.user.nao_saas };
+                localStorage.user = JSON.stringify($scope.user);
             }
             else {
                 if ($("#toast-container")[0] && $("#toast-container")[0].children.length) return;
