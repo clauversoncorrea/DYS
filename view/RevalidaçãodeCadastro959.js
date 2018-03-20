9b5fcced3ab1b18706cb5f7627e86e3d50956186
dys <douglasantunesbarreto@gmail.com>
2018-03-20 17:03:23 -0300

    tela revalidação

---
+++ view/RevalidaçãodeCadastro959.js
@@ -0,0 +1,1 @@
+app.controller('RevalidaçãodeCadastro959', function ($scope){ g$.configTela('Revalidação de Cadastro');g$.leTela("leTela | view | 2213 ", true);g$.alteraPropriedade("alteraPropriedade | 45229 | display | none | '»user.projeto_id»' = '3'", true);$("[data-id=45234]")[0].addEventListener("click", g$.salvarTela.bind(null, "salvarTela | view | ", false), false);});
