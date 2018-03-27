49e7d57febb951db612991de637bb61fcdb1a44f
dys <douglasantunesbarreto@gmail.com>
2018-03-27 20:14:21 -0300

    url

--- controller/app.js
+++ controller/app.js
@@ -4,7 +4,7 @@ var banco_node = (ambiente == "") ? "node" : "node_" + ambiente;
 var banco_saude = (ambiente == "") ? "saude" : "saude_" + ambiente;
 var port = (location.href.indexOf("treinamento") > -1 ? "8020" : (location.href.indexOf("homologacao") > -1) ? "8010" : "8000");
 
-var URL = "http://192.168.66.19/" + (ambiente == "") ? "/" : "";
+var URL = "http://192.168.66.19/" + ((ambiente == "") ? "/" : ambiente);
 // local
 // var URL = "" + ((ambiente == "") ? "/" : "");
 
--- controller/notification.js
+++ controller/notification.js
@@ -27,7 +27,7 @@ var app2 = angular.module('myApp', ['ionic'])
 
     });
 
-var URL = "http://192.168.66.19/" + (ambiente == "") ? "/" : "";
+var URL = "http://192.168.66.19/" + ((ambiente == "") ? "/" : ambiente);
 // local
 // var URL = "" + ((ambiente == "") ? "/" : "");
 const syek = {
