7060d1330cbc645c2f6682ba9747d94a4508fffd
dys <douglasantunesbarreto@gmail.com>
2018-03-27 22:33:16 -0300

    modals

--- modals/modalEncaminhamentos1065.js
+++ modals/modalEncaminhamentos1065.js
@@ -1,1 +1,1 @@
-app.directive('modalencaminhamentos1065', function () {return {restrict: 'E',templateUrl: '../modals/modalEncaminhamentos1065.html', scope: {}, controller: function ($scope, $element, $http, $compile, $rootScope) {g$.configTela('Encaminhamentos');}}});
+app.directive('modalencaminhamentos1065', function () {return {restrict: 'E',templateUrl: 'modals/modalEncaminhamentos1065.html', scope: {}, controller: function ($scope, $element, $http, $compile, $rootScope) {g$.configTela('Encaminhamentos');}}});
