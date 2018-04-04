9b2906213143dc6cfe3c903b3a1574088624b54e
Douglas Antunes <DouglasAntunesBarreto@gmail.com>
2018-04-04 00:36:52 -0300

    url

--- temp/template.js
+++ temp/template.js
@@ -9,7 +9,7 @@ var banco_saude = (ambiente == "") ? "saude" : "saude_" + ambiente;
 var port = (location.href.indexOf("treinamento") > -1 ? "8020" : (location.href.indexOf("homologacao") > -1) ? "8010" : "8000");
 var URL;
 if(location.href.indexOf("localhost") > -1) URL = "" + ((ambiente == "") ? "/" : "");
-else URL = "http://192.168.66.19/" + ((ambiente == "") ? "/" : ambiente);
+else URL = "http://192.168.66.19/" + ((ambiente == "") ? "" : ambiente);
 
 g$.urlObj = function (url) {
     var url = url.split("?")[1],
